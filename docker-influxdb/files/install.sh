fd424f34f800eab384f4de075c4d60d5220215ef
root <root@x.x.n13.cz>
2016-03-08 21:00:22 +0100

    .

--- docker-influxdb/files/install.sh
+++ docker-influxdb/files/install.sh
@@ -1,4 +1,5 @@
 #/usr/bin/env sh
+set -ev
 
 export DEV_PACKAGES="build-base make git go@edge"
 
@@ -13,10 +14,19 @@ cd $GOPATH/src/github.com/influxdb
 
 # download and clone influxdb
 git clone --branch ${INFLUXDB_VERSION} https://github.com/influxdb/influxdb.git
+
+exit 0
+
 go get -u -f -t ./...
 go build -ldflags="-X main.version=${INFLUXDB_VERSION} -X main.branch=master -X main.commit=`cd influxdb && git rev-parse HEAD`" ./...
 go install ./...
 cp $GOPATH/bin/* /usr/local/bin/
+
+# prepare dir structure and place-in configs
+mkdir -p /etc/influxdb
+mkdir -p /data/influxdb
+mv /tmp/collectd.types.db /usr/share/collectd/types.db
+mv /tmp/config.toml /etc/influxdb/config.toml
 
 # clean up
 apk del $DEV_PACKAGES
