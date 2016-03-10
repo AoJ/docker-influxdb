7b9050c55fe6b2d66f0f1a0d2aa7606761b2e1b5
aoj <aoj@n13.cz>
2016-03-10 09:09:25 +0100

    final 0.10.2

--- docker-influxdb/README.md
+++ docker-influxdb/README.md
@@ -1,14 +1,14 @@
-# Tinc for Docker
+# Influxdb in container
 
-only 6mb size!
+- only 35mb size!
+- default config
+- based on alpine linux
+- run under user influx (1301)
+- with tini supervizor for proper manager SIG*
+- data dir is in /var/lib/influxdb
+- config in /etc/influxdb.conf
 
 ## Usage
 ```
-docker run -d \
-    --name tinc \
-    --net=host \
-    --device=/dev/net/tun \
-    --cap-add NET_ADMIN \
-    --volume /srv/tinc:/etc/tinc \
-    aooj/tinc start -D
+docker run -p 8083:8083 -p 8086:8086 -d aooj/influxdb:v0.10.2
 ```
--- docker-influxdb/files/install.sh
+++ docker-influxdb/files/install.sh
@@ -2,7 +2,7 @@
 set -evx
 cd /tmp
 export INFLUXDB_GIT_URL=https://github.com/influxdata/influxdb.git
-export GOLANG_URL=https://golang.org/dl/go$GOLANG_VERSION.src.tar.gz
+export GO_URL=https://golang.org/dl/go$GOLANG_VERSION.src.tar.gz
 
 # install build deps
 apk add --update --virtual .build-deps \
@@ -14,36 +14,62 @@ apk add --update --virtual .build-deps \
         git
 
 # download go
-wget -q "$GOLANG_URL" -O golang.tar.gz
-echo "$GOLANG_SHA1  golang.tar.gz" | sha1sum -c -
-tar -C /usr/local -xzf golang.tar.gz
-cd /usr/local/go/src && ./make.bash
+wget -q "$GO_URL" -O go.tar.gz
+echo "$GOLANG_SHA1  go.tar.gz" | sha1sum -c -
+tar -C /usr/local -xzf go.tar.gz
+cd /usr/local/go/src
+./make.bash
 
-# download influxdb
-export GOPATH=/tmp
+# prepare for download and build
+export GOPATH=/go
 export GOROOT=/usr/local/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
-mkdir -p $GOPATH/src/github.com/influxdata
-cd $GOPATH/src/github.com/influxdata
-git clone --depth=1 -q --branch $INFLUXDB_VERSION $INFLUXDB_GIT_URL
-cd $GOPATH/src/github.com/influxdata/influxdb
-
-# build
+# download influxdb and deps
+go get github.com/influxdata/influxdb
+cd $GOPATH/src/github.com
+ln -s influxdata influxdb #bug with old influxdb name
+cd influxdata/influxdb
 go get -u -f -t ./...
 go clean ./...
+
+# build
+git checkout -q --detach "v$INFLUXDB_VERSION"
+# use date from last commit in branch instead of current date
 date=$(git log -n1 --format="%aI" --date=iso8601-strict)
-LDFLAGS="-X main.version $INFLUXDB_VERSION"                      \
-LDFLAGS="$LDFLAGS -X main.branch master"                         \
-LDFLAGS="$LDFLAGS -X main.commit $(git rev-parse --short HEAD)"  \
-LDFLAGS="$LDFLAGS -X main.buildTime $date"                       \
-        go install -ldflags="$LDFLAGS" ./...
-cp $GOPATH/bin/influx* /usr/bin/
+export LDFLAGS="-X main.version $INFLUXDB_VERSION"
+export LDFLAGS="$LDFLAGS -X main.branch master"
+export LDFLAGS="$LDFLAGS -X main.commit $(git rev-parse --short HEAD)"
+export LDFLAGS="$LDFLAGS -X main.buildTime $date"
+go install -ldflags="$LDFLAGS" ./...
+
+# copy final bin
+if [[ -z "$KEEP_DEBUG_TOOLS" ]]; then
+  files="$GOPATH/bin/influx $GOPATH/bin/influxd"
+else
+  files=$(ls $GOPATH/bin/*)
+fi
+cp $files /usr/local/bin/
+chown influx:influx $files
+chmod 550 $files
+
+
+# use default config
+cp etc/config.sample.toml /etc/influxdb.conf
+chown influx:influx /etc/influxdb.conf
+chmod 440 /etc/influxdb.conf
+
+# prepare data dir
+mkdir -p /var/lib/influxdb
+chown -R influx:influx /var/lib/influxdb
+chmod -R 772 /var/lib/influxdb
+
+# prepare data dir
+mkdir -p /run/influx
+chown -R influx:influx /run/influx
+chmod -R 770 /run/influx
 
 # clean up
 apk del .build-deps
 rm -rf /var/cache/apk/* /tmp/* /var/tmp/* $GOROOT $GOPATH
-
-# download and clone influxdb
-# git clone --branch ${INFLUXDB_VERSION} https://github.com/influxdb/influxdb.git
 
